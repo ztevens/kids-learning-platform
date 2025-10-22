b1d7bcd59765cb2f2c00c0a67c0b8047c78921f6
Stevens <109077037+ztevens@users.noreply.github.com>
2025-10-22 08:43:08 +0100

    Fix environment variable for Supabase anon key

--- lib/supabase/server.ts
+++ lib/supabase/server.ts
@@ -11,7 +11,7 @@ export async function createClient() {
 
   return createServerClient(
     process.env.SUPABASE_NEXT_PUBLIC_SUPABASE_URL!,
-    proSUPABASE_NEXT_PUBLIC_SUPABASE_ANON_KEY_ANON_KEY!,
+    process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY!,
     {
       cookies: {
         getAll() {
